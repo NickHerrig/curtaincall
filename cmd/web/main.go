e20b44230ecc7029750e2a1b562a4c6468654860
nick <neherrig@gmail.com>
2020-04-12 20:09:42 +0000

    Add flag for port, and add info/error logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,19 +1,33 @@
 package main
 
 import (
+    "flag"
     "log"
     "net/http"
+    "os"
 )
 
 func main() {
+
+    addr := flag.String("addr", ":8080", "HTTP network address")
+    flag.Parse()
+
+    infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+    errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
     mux := http.NewServeMux()
     mux.HandleFunc("/", home)
 
     fileServer := http.FileServer(http.Dir("./ui/static/"))
     mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
+    srv := &http.Server{
+        Addr:     *addr,
+        ErrorLog: errorLog,
+        Handler:  mux,
+    }
 
-    log.Println("Starting Curtain Call on port :9000")
-    err := http.ListenAndServe(":9000", mux)
-    log.Fatal(err)
+    infoLog.Printf("Starting Curtain Call on port %s", *addr)
+    err := srv.ListenAndServe()
+    errorLog.Fatal(err)
 }
