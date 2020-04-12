f1eeb3184f7176324b6fe5c4acec0305d046fd3a
nick <neherrig@gmail.com>
2020-04-12 20:27:13 +0000

    Dependency injection with appication struct

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,11 +2,10 @@ package main
 
 import (
     "html/template"
-    "log"
     "net/http"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
     if r.URL.Path != "/" {
         http.NotFound(w, r)
         return
@@ -14,14 +13,14 @@ func home(w http.ResponseWriter, r *http.Request) {
 
     ts, err := template.ParseFiles("./ui/html/home.page.tmpl")
     if err != nil {
-        log.Println(err.Error())
+        app.errorLog.Println(err.Error())
         http.Error(w, "Internal Server Error", 500)
         return
     }
 
     err = ts.Execute(w, nil)
     if err != nil {
-        log.Println(err.Error())
+        app.errorLog.Println(err.Error())
         http.Error(w, "Internal Server Error", 500)
     }
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,11 @@ import (
     "os"
 )
 
+type application struct {
+    errorLog *log.Logger
+    infoLog  *log.Logger
+}
+
 func main() {
 
     addr := flag.String("addr", ":8080", "HTTP network address")
@@ -15,8 +20,13 @@ func main() {
     infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
     errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+    app := &application{
+        errorLog: errorLog,
+        infoLog:  infoLog,
+    }
+
     mux := http.NewServeMux()
-    mux.HandleFunc("/", home)
+    mux.HandleFunc("/", app.home)
 
     fileServer := http.FileServer(http.Dir("./ui/static/"))
     mux.Handle("/static/", http.StripPrefix("/static", fileServer))
