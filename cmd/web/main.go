5d98bc046e38715377b7549a324489f218f3c553
nick <neherrig@gmail.com>
2020-04-13 02:21:00 +0000

    Install/Setup of sqlite3 driver

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+    "database/sql"
     "flag"
     "log"
     "net/http"
     "os"
+
+    _ "github.com/mattn/go-sqlite3"
 )
 
 type application struct {
@@ -15,10 +18,17 @@ type application struct {
 func main() {
 
     addr := flag.String("addr", ":8080", "HTTP network address")
+    dsn := flag.String("dsn", "./test.db", "Database data source name")
     flag.Parse()
 
     infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
     errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+    database, err := sql.Open("sqlite3", *dsn)
+    if err != nil {
+        errorLog.Fatal(err) 
+    }
+    defer database.Close()
 
     app := &application{
         errorLog: errorLog,
@@ -32,6 +42,6 @@ func main() {
     }
 
     infoLog.Printf("Starting Curtain Call on port %s", *addr)
-    err := srv.ListenAndServe()
+    err = srv.ListenAndServe()
     errorLog.Fatal(err)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module curtaincall.tech
 
 go 1.14
+
+require github.com/mattn/go-sqlite3 v2.0.3+incompatible
