1894dc887297c00e505b270aad486f41855b1538
nick <neherrig@gmail.com>
2020-04-15 03:41:49 +0000

    Theater model started, needs injected in app

---
+++ pkg/models/models.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "erros"
+    "Time"
+) 
+
+ErrNoRecord = errors.New("models: no matching record found")
+
+type Theater struct {
+    ID      int
+    name    string
+}
+
+type Show struct {
+    ID      int
+    name    string
+    company string
+}
+
+type Song struct {
+    ID          int
+    name        string
+    number      int
+    actNumber   int
+}
---
+++ pkg/models/sqlite/theaters.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "database/sql"
+
+    "curtaincall.tech/pkg/models"
+)
+
+type TheaterModel struct {
+    DB *sql.DB
+}
+
+func (m *TheaterModel) Insert(name string) (int, error) {
+    return 0, nil
+}
+
+func (m *TheaterModel) Get(id int) (*models.Theater, error) {
+    return nil, nil
+}
