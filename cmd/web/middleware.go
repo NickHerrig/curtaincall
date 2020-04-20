d19b72ef90a6769ae13285469d253018fd03f163
nick <neherrig@gmail.com>
2020-04-20 13:11:20 +0000

    Add request logging of ever request with middleware

---
+++ cmd/web/middleware.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "net/http"
+)
+
+func secureHeaders(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("X-XSS-Protection", "1; mode=block")
+        w.Header().Set("X-Frame-Options", "deny")
+
+        next.ServeHTTP(w, r)
+    })
+}
+
+func (app *application) logRequest(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
+
+        next.ServeHTTP(w, r)
+    })
+}
