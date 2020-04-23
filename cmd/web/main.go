7f2b7ea1ec25916091fbd6e58d8963e329b5451d
nick <neherrig@gmail.com>
2020-04-23 05:09:15 +0000

    Configure https settings with tlsconfig struct

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "crypto/tls"
     "database/sql"
     "flag"
     "html/template"
@@ -57,10 +58,16 @@ func main() {
         templateCache: templateCache,
     }
 
+    tlsConfig := &tls.Config{
+        PreferServerCipherSuites: true,
+        CurvePreferences:         []tls.CurveID{tls.X25519, tls.CurveP256},
+    }
+
     srv := &http.Server{
-        Addr:     *addr,
-        ErrorLog: errorLog,
-        Handler:  app.routes(),
+        Addr:      *addr,
+        ErrorLog:  errorLog,
+        Handler:   app.routes(),
+        TLSConfig: tlsConfig,
     }
 
     infoLog.Printf("Starting Curtain Call on port %s", *addr)
