5a92f8df2ffc7e5638c6d61efa42fe8bedf5e4d6
nick <neherrig@gmail.com>
2020-04-25 16:17:19 +0000

    Add user singup page and form validation

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -80,11 +80,31 @@ func (app *application) createTheater(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) signupUserForm(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "GET: Display user signup form.")
+    app.render(w, r, "signup.page.tmpl", &templateData{
+        Form: forms.New(nil),
+        })
 }
 
 func (app *application) signupUser(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "POST: Signup User.")
+    err := r.ParseForm()
+    if err != nil {
+        app.clientError(w, http.StatusBadRequest)
+        return
+    }
+
+    form := forms.New(r.PostForm)
+    form.Required("name", "email", "password")
+    form.MaxLength("name", 255)
+    form.MaxLength("email", 255)
+    form.MatchesPattern("email", forms.EmailRX)
+    form.MinLength("password", 10)
+
+    if !form.Valid() {
+        app.render(w, r, "signup.page.tmpl", &templateData{Form: form})
+        return
+    }
+
+    fmt.Fprintln(w, "Create a new user...")
 }
 
 func (app *application) loginUserForm(w http.ResponseWriter, r *http.Request) {
--- pkg/forms/form.go
+++ pkg/forms/form.go
@@ -1,9 +1,14 @@
 package forms
 
 import (
+    "fmt"
     "net/url"
+    "regexp"
     "strings"
+    "unicode/utf8"
 )
+
+var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
 type Form struct {
     url.Values
@@ -23,6 +28,36 @@ func (f *Form) Required(fields ...string) {
         if strings.TrimSpace(value) == "" {
             f.Errors.Add(field, "This field cannot be blank")
         }
+    }
+}
+
+func (f *Form) MaxLength(field string, d int) {
+    value := f.Get(field)
+    if value == "" {
+        return
+    }
+    if utf8.RuneCountInString(value) > d {
+        f.Errors.Add(field, fmt.Sprintf("This field is too long (maximum is %d characters)", d))
+    }
+}
+
+func (f *Form) MinLength(field string, d int) {
+    value := f.Get(field)
+    if value == "" {
+        return
+    }
+    if utf8.RuneCountInString(value) < d {
+        f.Errors.Add(field, fmt.Sprintf("This field is too short (minimum is %d characters)", d))
+    }
+}
+
+func (f *Form) MatchesPattern(field string, pattern *regexp.Regexp) {
+    value := f.Get(field)
+    if value == "" {
+        return
+    }
+    if !pattern.MatchString(value) {
+        f.Errors.Add(field, "This field is invalid")
     }
 }
 
