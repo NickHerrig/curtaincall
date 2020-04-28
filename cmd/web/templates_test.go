d3242fb6d8835d7bd33320e27786bda2f607bc86
nick <neherrig@gmail.com>
2020-04-28 01:33:49 +0000

    Add table-Driven Tests for humanDate() func

--- cmd/web/templates_test.go
+++ cmd/web/templates_test.go
@@ -6,9 +6,35 @@ import (
 )
 
 func TestHumanDate(t *testing.T) {
-    tm := time.Date(2020, 12, 17, 10, 0, 0, 0, time.UTC)
-    hd := humanDate(tm)
-    if hd != "17 Dec 2020 at 10:00" {
-        t.Errorf("want %q: got %q", "17 Dec 2020 at 10:00", hd)
+
+    tests := []struct {
+        name string
+        tm   time.Time
+        want string
+    }{
+         {
+            name: "UTC",
+            tm:   time.Date(2020, 12, 17, 10, 0, 0, 0, time.UTC),
+            want: "17 Dec 2020 at 10:00",
+         },
+         {
+            name: "Empty",
+            tm:   time.Time{},
+            want: "",
+         },
+         {
+            name: "CET",
+            tm:   time.Date(2020, 12, 17, 10, 0, 0, 0, time.FixedZone("CET", 1*60*60)),
+            want: "17 Dec 2020 at 09:00",
+         },
+    }
+
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            hd := humanDate(tt.tm)
+            if hd != tt.want {
+                t.Errorf("want %q: got %q", tt.want, hd)
+            }
+        })
     }
 }
