ea5f738e213976858f00d393cab1a4406198e852
nick <neherrig@gmail.com>
2020-05-01 04:31:52 +0000

    Testing html forms and Post requests

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -3,6 +3,7 @@ package main
 import (
     "bytes"
     "net/http"
+    "net/url"
     "testing"
 )
 
@@ -46,11 +47,60 @@ func TestShowTheater(t *testing.T) {
         t.Run(tt.name, func(t *testing.T) {
             code, _, body := ts.get(t, tt.urlPath)
 
-            if code != tt.wantCode{
+            if code != tt.wantCode {
                 t.Errorf("want %d; got %d", tt.wantCode, code)
             }
             if !bytes.Contains(body, tt.wantBody) {
                 t.Errorf("want %d; got %d", tt.wantBody, body)
+            }
+        })
+    }
+}
+
+func TestSignupUser(t *testing.T) {
+    app := newTestApplication(t)
+    ts := newTestServer(t, app.routes())
+    defer ts.Close()
+
+    _, _, body := ts.get(t, "/user/signup")
+    csrfToken := extractCSRFToken(t, body)
+
+    tests := []struct {
+        name         string
+        userName     string
+        userEmail    string
+        userPassword string
+        csrfToken    string
+        wantCode     int
+        wantBody     []byte
+    }{
+         {"Valid submission", "nick", "nick@gmail.com", "validPa$$word", csrfToken, http.StatusSeeOther, nil},
+         {"Empty name", "", "nick@gmail.com", "validPa$$word", csrfToken, http.StatusOK, []byte("This field cannot be blank")},
+         {"Empty email", "nick", "", "validPa$$word", csrfToken, http.StatusOK, []byte("This field cannot be blank")},
+         {"Empty password", "nick", "nick@gmail.com", "", csrfToken, http.StatusOK, []byte("This field cannot be blank")},
+         {"Invalid email (incomplete domain)", "nick", "nick@gmail.", "validPa$$word", csrfToken, http.StatusOK, []byte("This field is invalid")},
+         {"Invalid email (missing @)", "nick", "nickgmail.com", "validPa$$word", csrfToken, http.StatusOK, []byte("This field is invalid")},
+         {"Invalid email (missing local part)", "nick", "@gmail.com", "validPa$$word", csrfToken, http.StatusOK, []byte("This field is invalid")},
+         {"Short password", "nick", "nick@gmail.com", "Pass", csrfToken, http.StatusOK, []byte("This field is too short")},
+         {"Duplicate email", "nick", "dupe@example.com", "validPa$$word", csrfToken, http.StatusOK, []byte("Address is already in use")},
+         {"Invalid CSRF Token", "", "", "", "wrongtoken", http.StatusBadRequest, nil},
+     }
+
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            form := url.Values{}
+            form.Add("name", tt.userName)
+            form.Add("email", tt.userEmail)
+            form.Add("password", tt.userPassword)
+            form.Add("csrf_token", tt.csrfToken)
+
+            code, _, body := ts.postForm(t, "/user/signup", form)
+
+            if code != tt.wantCode {
+                t.Errorf("want %d; got %d", tt.wantCode, code)
+            }
+            if !bytes.Contains(body, tt.wantBody) {
+                t.Errorf("want %q; got %s", tt.wantBody, body)
             }
         })
     }
--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+    "html"
     "io/ioutil"
     "log"
     "net/http"
     "net/http/cookiejar"
     "net/http/httptest"
+    "net/url"
+    "regexp"
     "testing"
     "time"
 
@@ -13,6 +16,17 @@ import (
 
     "github.com/golangcollege/sessions"
 )
+
+var csrfTokenRX = regexp.MustCompile(`<input type='hidden' name='csrf_token' value='(.+)'>`)
+
+func extractCSRFToken(t *testing.T, body []byte) string {
+    matches := csrfTokenRX.FindSubmatch(body)
+    if len(matches) < 2 {
+        t.Fatal("No csrf token found in body")
+    }
+
+    return html.UnescapeString(string(matches[1]))
+}
 
 func newTestApplication(t *testing.T) *application {
     templateCache, err := newTemplateCache("./../../ui/html/")
@@ -57,6 +71,20 @@ func newTestServer(t *testing.T, h http.Handler) *testServer {
 
 func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, []byte) {
     rs, err := ts.Client().Get(ts.URL + urlPath)
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    defer rs.Body.Close()
+    body, err := ioutil.ReadAll(rs.Body)
+    if err != nil {
+        t.Fatal(err)
+    }
+    return rs.StatusCode, rs.Header, body
+}
+
+func (ts *testServer) postForm(t *testing.T, urlPath string, form url.Values) (int, http.Header, []byte) {
+    rs, err := ts.Client().PostForm(ts.URL + urlPath, form)
     if err != nil {
         t.Fatal(err)
     }
