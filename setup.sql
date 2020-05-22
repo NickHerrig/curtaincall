fbe4effa0608076037d329358915e65de4b72ffe
neherrig <neherrig@gmail.com>
2020-05-22 01:32:43 -0500

    update setup script..

--- setup.sql
+++ setup.sql
@@ -42,45 +42,45 @@ VALUES ("Mean Girls", "Company C");
 
 /* Setup Mean Girls Songs */
 INSERT INTO songs (song_name, song_number, song_act_number, show_id)
-VALUES ("A Cautionary Tale", 1, 1, 3)
+VALUES ("A Cautionary Tale", 1, 1, 3);
 INSERT INTO songs (song_name, song_number, song_act_number, show_id)
-VALUES ("It Roars", 2, 1, 3)
+VALUES ("It Roars", 2, 1, 3);
 INSERT INTO songs (song_name, song_number, song_act_number, show_id)
-VALUES ("Where Do You Belong?", 3, 1, 3)
+VALUES ("Where Do You Belong?", 3, 1, 3);
 INSERT INTO songs (song_name, song_number, song_act_number, show_id)
-VALUES ("Meet the Plastics", 4, 1, 3)
+VALUES ("Meet the Plastics", 4, 1, 3);
 INSERT INTO songs (song_name, song_number, song_act_number, show_id)
-VALUES ("Stupid with Love", 5, 1, 3)
+VALUES ("Stupid with Love", 5, 1, 3);
 INSERT INTO songs (song_name, song_number, song_act_number, show_id)
-VALUES ("Apex Predator", 6, 1, 3)
+VALUES ("Apex Predator", 6, 1, 3);
 INSERT INTO songs (song_name, song_number, song_act_number, show_id)
-VALUES ("What's Wrong with Me?", 7, 1, 3)
+VALUES ("What's Wrong with Me?", 7, 1, 3);
 INSERT INTO songs (song_name, song_number, song_act_number, show_id)
-VALUES ("Sexy", 8, 1, 3)
+VALUES ("Sexy", 8, 1, 3);
 INSERT INTO songs (song_name, song_number, song_act_number, show_id)
-VALUES ("Someone Gets Hurt", 9, 1, 3)
+VALUES ("Someone Gets Hurt", 9, 1, 3);
 INSERT INTO songs (song_name, song_number, song_act_number, show_id)
-VALUES ("Revenge Party", 10, 1, 3)
+VALUES ("Revenge Party", 10, 1, 3);
 INSERT INTO songs (song_name, song_number, song_act_number, show_id)
-VALUES ("Fearless", 11, 1, 3)
+VALUES ("Fearless", 11, 1, 3);
 INSERT INTO songs (song_name, song_number, song_act_number, show_id)
-VALUES ("Stop", 12, 2, 3)
+VALUES ("Stop", 12, 2, 3);
 INSERT INTO songs (song_name, song_number, song_act_number, show_id)
-VALUES ("What's Wrong with Me? (Reprise)", 13, 2, 3)
+VALUES ("What's Wrong with Me? (Reprise)", 13, 2, 3);
 INSERT INTO songs (song_name, song_number, song_act_number, show_id)
-VALUES ("Whose House Is This?", 14, 2, 3)
+VALUES ("Whose House Is This?", 14, 2, 3);
 INSERT INTO songs (song_name, song_number, song_act_number, show_id)
-VALUES ("More Is Better", 15, 2, 3)
+VALUES ("More Is Better", 15, 2, 3);
 INSERT INTO songs (song_name, song_number, song_act_number, show_id)
-VALUES ("World Burn", 16, 2, 3)
+VALUES ("World Burn", 16, 2, 3);
 INSERT INTO songs (song_name, song_number, song_act_number, show_id)
-VALUES ("I'd Rather Be Me", 17, 2, 3)
+VALUES ("I'd Rather Be Me", 17, 2, 3);
 INSERT INTO songs (song_name, song_number, song_act_number, show_id)
-VALUES ("Fearless (Reprise)", 18, 2, 3)
+VALUES ("Fearless (Reprise)", 18, 2, 3);
 INSERT INTO songs (song_name, song_number, song_act_number, show_id)
-VALUES ("Do This Thing", 19, 2, 3)
+VALUES ("Do This Thing", 19, 2, 3);
 INSERT INTO songs (song_name, song_number, song_act_number, show_id)
-VALUES ("I See Stars", 20, 2, 3)
+VALUES ("I See Stars", 20, 2, 3);
 
 /* Setup Show and Theater Many-to-many relationships */
 INSERT INTO theaters_shows_bridge (theater_id, show_id)
