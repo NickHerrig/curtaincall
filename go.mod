a29e4c4c13568556ba99341a41abe8ecde05ea5a
nickherrig <neherrig@gmail.com>
2020-06-05 08:09:46 -0500

    Added Content-Type check, and malformed json body

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/bmizerany/pat v0.0.0-20170815010413-6226ea591a40
 	github.com/coreos/go-systemd v0.0.0-20191104093116-d3cd4ed1dbcf
+	github.com/golang/gddo v0.0.0-20200604155040-845892271f91
 	github.com/golangcollege/sessions v1.2.0
 	github.com/jinzhu/gorm v1.9.12
 	github.com/justinas/alice v1.2.0
--- pkg/http/rest/handler.go
+++ pkg/http/rest/handler.go
@@ -1,195 +1,226 @@
 package rest
 
 import (
-    "encoding/json"
-    "errors"
-    "net/http"
-    "strconv"
+	"encoding/json"
+	"errors"
+    "fmt"
+    "log"
+	"net/http"
+	"strconv"
 
-    "curtaincall.tech/pkg/creating"
-    "curtaincall.tech/pkg/retrieving"
-    "curtaincall.tech/pkg/deleting"
-    "curtaincall.tech/pkg/storage/sqlite"
+	"curtaincall.tech/pkg/creating"
+	"curtaincall.tech/pkg/deleting"
+	"curtaincall.tech/pkg/retrieving"
+	"curtaincall.tech/pkg/storage/sqlite"
 
-    "github.com/bmizerany/pat"
+	"github.com/bmizerany/pat"
+	"github.com/golang/gddo/httputil/header"
 )
 
 func Handler(c creating.Service, r retrieving.Service, d deleting.Service) http.Handler {
-    router := pat.New()
+	router := pat.New()
 
-    router.Post("/theaters", http.HandlerFunc(createTheater(c)))
-    router.Get("/theaters", http.HandlerFunc(retrieveAllTheaters(r)))
-    router.Get("/theaters/:id", http.HandlerFunc(retrieveTheater(r)))
-    router.Del("/theaters/:id", http.HandlerFunc(deleteTheater(d)))
+	router.Post("/theaters", http.HandlerFunc(createTheater(c)))
+	router.Get("/theaters", http.HandlerFunc(retrieveAllTheaters(r)))
+	router.Get("/theaters/:id", http.HandlerFunc(retrieveTheater(r)))
+	router.Del("/theaters/:id", http.HandlerFunc(deleteTheater(d)))
 
-    router.Get("/theaters/:id/shows", http.HandlerFunc(retrieveAllShows(r)))
-    router.Get("/theaters/:id/shows/:showid", http.HandlerFunc(retrieveShow(r)))
-    // router.Post("/shows", http.HandlerFunc(createShow(c)))
+	router.Get("/theaters/:id/shows", http.HandlerFunc(retrieveAllShows(r)))
+	router.Get("/theaters/:id/shows/:showid", http.HandlerFunc(retrieveShow(r)))
+	router.Post("/shows", http.HandlerFunc(createShow(c)))
 
-    return router
+	return router
 }
 
 func createTheater(s creating.Service) func(w http.ResponseWriter, r *http.Request) {
-    return func(w http.ResponseWriter, r *http.Request) {
-        //var id int
-        decoder := json.NewDecoder(r.Body)
+	return func(w http.ResponseWriter, r *http.Request) {
 
-        var newTheater creating.Theater
-        err := decoder.Decode(&newTheater)
-        if err != nil {
-            http.Error(w, err.Error(), http.StatusBadRequest)
+        // This chunk checks to see that that Contentp-Type header is present
+        // and that it is set to application/json
+		if r.Header.Get("Content-Type") != "" {
+			value, _ := header.ParseValueAndParams(r.Header, "Content-Type")
+			if value != "application/json" {
+				msg := "Content-Type header is not application/json"
+				http.Error(w, msg, http.StatusUnsupportedMediaType)
+				return
+			}
+		}
+
+        // this chunk enforces a maximum 1MB body size
+        // Decode() will return "http: request body too large" an HTTP 413
+        r.Body = http.MaxBytesReader(w, r.Body, 1048576)
+
+        // decode the body and call DisallowUnknownFields() on it.
+        // this will cause Decode() to raise a 
+		dec := json.NewDecoder(r.Body)
+        dec.DisallowUnknownFields()
+
+		var t creating.Theater
+		err := dec.Decode(&t)
+		if err != nil {
+            var syntaxError *json.SyntaxError
+        
+            switch {
+            case errors.As(err, &syntaxError):
+                msg := fmt.Sprintf("Request body contains malformed JSON (at position %d)", syntaxError.Offset)
+                http.Error(w, msg, http.StatusBadRequest)
+
+            default:
+                log.Println(err.Error())
+                http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+            }
             return
-        }
+		}
+		_, err = s.CreateTheater(t)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
 
-        _, err = s.CreateTheater(newTheater)
-        if err != nil {
-            http.Error(w, err.Error(), http.StatusBadRequest)
-            return
-        }
-
-        //SET HEADERS IN MIDDLEWARE
-        w.Header().Set("Content-Type", "application/json")
-        json.NewEncoder(w).Encode("New Theater Created!")
-    }
+		//SET HEADERS IN MIDDLEWARE
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode("New Theater Created!")
+	}
 }
 
 func createShow(s creating.Service) func(w http.ResponseWriter, r *http.Request) {
-    return func(w http.ResponseWriter, r *http.Request) {
-        //var id int
-        decoder := json.NewDecoder(r.Body)
+	return func(w http.ResponseWriter, r *http.Request) {
+		//var id int
+		decoder := json.NewDecoder(r.Body)
 
-        var newShow creating.Show
+		var newShow creating.Show
 
-        err := decoder.Decode(&newShow)
-        if err != nil {
-            http.Error(w, err.Error(), http.StatusBadRequest)
-            return
-        }
+		err := decoder.Decode(&newShow)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
 
-        _, err = s.CreateShow(newShow)
-        if err != nil {
-            http.Error(w, err.Error(), http.StatusBadRequest)
-            return
-        }
+		_, err = s.CreateShow(newShow)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
 
-        //SET HEADERS IN MIDDLEWARE
-        w.Header().Set("Content-Type", "application/json")
-        json.NewEncoder(w).Encode("New Show Created")
-    }
+		//SET HEADERS IN MIDDLEWARE
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode("New Show Created")
+	}
 }
 
 func retrieveAllTheaters(s retrieving.Service) func(w http.ResponseWriter, r *http.Request) {
-    return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 
-        theaters, err := s.RetrieveAllTheaters()
-        if err != nil {
-            http.Error(w, err.Error(), http.StatusBadRequest)
-            return
-        }
+		theaters, err := s.RetrieveAllTheaters()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
 
-        //SET HEADERS IN MIDDLEWARE
-        w.Header().Set("Content-Type", "application/json")
-        json.NewEncoder(w).Encode(theaters)
-    }
+		//SET HEADERS IN MIDDLEWARE
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(theaters)
+	}
 }
 
 func retrieveTheater(s retrieving.Service) func(w http.ResponseWriter, r *http.Request) {
-    return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 
-      id, err := strconv.Atoi(r.URL.Query().Get(":id"))
-      if err != nil {
-          http.Error(w, err.Error(), http.StatusNotFound)
-          return
-      }
+		id, err := strconv.Atoi(r.URL.Query().Get(":id"))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
 
-      theater, err := s.RetrieveTheater(id)
-      if err != nil {
-          if errors.Is(err, sqlite.ErrNoRecord) {
-              http.Error(w, err.Error(), http.StatusNotFound)
-          } else {
-              http.Error(w, err.Error(), http.StatusBadRequest)
-          }
-          return
-      }
-        
-      //SET HEADERS IN MIDDLEWARE
-      w.Header().Set("Content-Type", "application/json")
-      json.NewEncoder(w).Encode(theater)
-    }
+		theater, err := s.RetrieveTheater(id)
+		if err != nil {
+			if errors.Is(err, sqlite.ErrNoRecord) {
+				http.Error(w, err.Error(), http.StatusNotFound)
+			} else {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+			}
+			return
+		}
+
+		//SET HEADERS IN MIDDLEWARE
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(theater)
+	}
 }
 
 func deleteTheater(s deleting.Service) func(w http.ResponseWriter, r *http.Request) {
-    return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 
-      id, err := strconv.Atoi(r.URL.Query().Get(":id"))
-      if err != nil {
-          http.Error(w, err.Error(), http.StatusNotFound)
-          return
-      }
+		id, err := strconv.Atoi(r.URL.Query().Get(":id"))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
 
-      err = s.DeleteTheater(id)
-      if err != nil {
-          if errors.Is(err, sqlite.ErrNoRecord) {
-              http.Error(w, err.Error(), http.StatusNotFound)
-          } else {
-              http.Error(w, err.Error(), http.StatusBadRequest)
-          }
-          return
-      }
-        
-      //SET HEADERS IN MIDDLEWARE
-      w.Header().Set("Content-Type", "application/json")
-      json.NewEncoder(w).Encode("Theater Deleted!")
-    }
+		err = s.DeleteTheater(id)
+		if err != nil {
+			if errors.Is(err, sqlite.ErrNoRecord) {
+				http.Error(w, err.Error(), http.StatusNotFound)
+			} else {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+			}
+			return
+		}
+
+		//SET HEADERS IN MIDDLEWARE
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode("Theater Deleted!")
+		w.WriteHeader(http.StatusNoContent)
+	}
 }
 
 func retrieveAllShows(s retrieving.Service) func(w http.ResponseWriter, r *http.Request) {
-    return func(w http.ResponseWriter, r *http.Request) {
-      id, err := strconv.Atoi(r.URL.Query().Get(":id"))
-      if err != nil {
-          http.Error(w, err.Error(), http.StatusNotFound)
-          return
-      }
+	return func(w http.ResponseWriter, r *http.Request) {
+		id, err := strconv.Atoi(r.URL.Query().Get(":id"))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
 
-      shows, err := s.RetrieveAllShows(id)
-      if err != nil {
-          if errors.Is(err, sqlite.ErrNoRecord) {
-              http.Error(w, err.Error(), http.StatusNotFound)
-          } else {
-              http.Error(w, err.Error(), http.StatusBadRequest)
-          }
-          return
-      }
-        
-      //SET HEADERS IN MIDDLEWARE
-      w.Header().Set("Content-Type", "application/json")
-      json.NewEncoder(w).Encode(shows)
-    }
+		shows, err := s.RetrieveAllShows(id)
+		if err != nil {
+			if errors.Is(err, sqlite.ErrNoRecord) {
+				http.Error(w, err.Error(), http.StatusNotFound)
+			} else {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+			}
+			return
+		}
+
+		//SET HEADERS IN MIDDLEWARE
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(shows)
+	}
 }
 
 func retrieveShow(s retrieving.Service) func(w http.ResponseWriter, r *http.Request) {
-    return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 
-      id, err := strconv.Atoi(r.URL.Query().Get(":showid"))
-      if err != nil {
-          http.Error(w, err.Error(), http.StatusNotFound)
-          return
-      }
+		id, err := strconv.Atoi(r.URL.Query().Get(":showid"))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
 
-      json.NewEncoder(w).Encode(id)
-      
-      show, err := s.RetrieveShow(id)
-      if err != nil {
-          if errors.Is(err, sqlite.ErrNoRecord) {
-              http.Error(w, err.Error(), http.StatusNotFound)
-          } else {
-              http.Error(w, err.Error(), http.StatusBadRequest)
-          }
-          return
-      }
-        
-      //SET HEADERS IN MIDDLEWARE
-      w.Header().Set("Content-Type", "application/json")
-      json.NewEncoder(w).Encode(show)
-    }
+		json.NewEncoder(w).Encode(id)
+
+		show, err := s.RetrieveShow(id)
+		if err != nil {
+			if errors.Is(err, sqlite.ErrNoRecord) {
+				http.Error(w, err.Error(), http.StatusNotFound)
+			} else {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+			}
+			return
+		}
+
+		//SET HEADERS IN MIDDLEWARE
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(show)
+	}
 }
