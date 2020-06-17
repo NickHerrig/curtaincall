e2f7f6095a9444aac2b1a9327732565003d01905
nickherrig <neherrig@gmail.com>
2020-06-16 21:09:31 -0500

    Add ShowTile component with dummy data, no styling

--- frontend/src/Shows.vue
+++ frontend/src/Shows.vue
@@ -1,16 +1,60 @@
 <template>
-  <div class="shows">
+<div>
+  <div>
     <h1>Popular Shows Tonight!</h1>
   </div>
+
+  <div v-if="shows.length <= 0">
+    <p>Sorry there are no shows yet!</p>
+  </div>
+  <div v-else>
+    <ShowTile 
+      v-for="show in shows" 
+      v-bind:key="show.id" 
+      v-bind:name="show.name">
+      v-bind:company="show.company">
+      v-bind:description="show.description">
+    </ShowTile>
+  </div>
+</div>
 </template>
 
 <script>
-  export default {
-    name: 'Shows',
-  }
+
+import ShowTile from '@/components/ShowTile.vue'
+
+export default {
+  name: 'Shows',
+  components: {
+    ShowTile,
+  },
+  data() {
+    return {
+      shows: [
+        {
+          id: 1,
+          name: 'name1',
+          company: 'company1',
+          description: 'description1',
+        },
+        {
+          id: 2,
+          name: 'name2',
+          company: 'company2',
+          description: 'description2',
+        },
+        {
+          id: 3,
+          name: 'name3',
+          company: 'company3',
+          description: 'description3',
+        },
+      ],
+    }
+  },
+}
 
 </script>
 
 <style>
-
 </style>
---
+++ frontend/src/components/ShowTile.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+  <h1>{{ name }}</h1>
+  <h1>{{ company }}</h1>
+  <h1>{{ description }}</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'ShowTile',
+  props: {
+    name: String,
+    company: String,
+    description: String,
+  },
+}
+</script>
+
+
+<style scoped>
+
+</style>
