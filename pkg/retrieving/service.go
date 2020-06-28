cd2ae603906c8f083cc7305d20c6448f803a7b48
nickherrig <neherrig@gmail.com>
2020-06-28 15:58:03 -0500

    Remove access code to simplify, storage querries and services

--- pkg/retrieving/service.go
+++ pkg/retrieving/service.go
@@ -1,17 +1,11 @@
 package retrieving
 
 type Service interface {
-    RetrieveAllTheaters() ([]*Theater, error)
-    RetrieveTheater(int)  (*Theater, error)
     RetrieveAllShows() ([]*Show, error)
-    RetrieveShow(int)     (*Show, error)
 }
 
 type Repository interface {
-    RetrieveAllTheaters() ([]*Theater, error)
-    RetrieveTheater(int)  (*Theater, error)
     RetrieveAllShows() ([]*Show, error)
-    RetrieveShow(int)     (*Show, error)
 }
 
 type service struct {
@@ -22,18 +16,6 @@ func NewService(r Repository) Service {
     return &service{r}
 }
 
-func (s *service) RetrieveAllTheaters() ([]*Theater, error) {
-     return s.r.RetrieveAllTheaters()
-}
-
-func (s *service) RetrieveTheater(id int) (*Theater, error) {
-     return s.r.RetrieveTheater(id)
-}
-
 func (s *service) RetrieveAllShows() ([]*Show, error) {
      return s.r.RetrieveAllShows()
-}
-
-func (s *service) RetrieveShow(id int) (*Show, error) {
-     return s.r.RetrieveShow(id)
 }
