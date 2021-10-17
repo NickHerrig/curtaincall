2c7701d54897be33e45ba948356bcd1a846e95b5
nickherrig <neherrig@gmail.com>
2021-10-17 12:30:04 -0500

    Scrape cast member and role

--- scrape.py
+++ scrape.py
@@ -6,6 +6,7 @@ import datetime
 
 def main():
     '''Description of the function'''
+### Get Current Touring Shows ###
 #    now = datetime.datetime.now()
 #    last_week = now - datetime.timedelta(days=7)
 #    next_week = now + datetime.timedelta(days=7)
@@ -30,14 +31,27 @@ def main():
 #    show_links = [ BASE + link['href'] for link in links ]
 #    pprint(show_links)
 
-    SHOW_URL = "https://www.ibdb.com/tour-production/come-from-away-518408#Songs"
+### Get songs and performers ###
+#    SHOW_URL = "https://www.ibdb.com/tour-production/come-from-away-518408#Songs"
+#    res = requests.get(SHOW_URL)
+#    soup = BeautifulSoup(res.content, "html.parser")
+#    song_id = soup.find("div", id=re.compile("Songs"))
+#    songs = song_id.find_all("div", {"class": "col s6"})
+#    title = [ song.text.strip() for song in songs ]
+#    song_performers = tuple(zip(title[::2], title[1::2]))
+#    pprint(song_performers)
+
+    SHOW_URL = "https://www.ibdb.com/tour-production/come-from-away-518408#CurrentCast"
     res = requests.get(SHOW_URL)
     soup = BeautifulSoup(res.content, "html.parser")
-    song_id = soup.find("div", id=re.compile("Songs"))
-    songs = song_id.find_all("div", {"class": "col s6"})
-    title = [ song.text.strip() for song in songs ]
-    song_performers = tuple(zip(title[::2], title[1::2]))
-    pprint(song_performers)
+    cast_id = soup.find("div", id=re.compile("CurrentCast"))
+    cast = cast_id.find_all("div", {"class": "row mobile-a-align"})
+    for c in cast:
+        cast_mems = c.find_all("div", {"class": "col m4 s12"})
+        cast =  cast_mems[0].text.strip()
+        role =  cast_mems[1].text.strip()
+        print(cast, role)
+
 
 if __name__ == '__main__':
     main()
