63c691be88a7e473d7fe4022bdea748e0a20fcb6
nickherrig <neherrig@gmail.com>
2021-10-17 11:58:03 -0500

    scrape a particular link for songs and performers

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Curtain Call is a responsive web application written in go.
 The application is meant to be an digital alertnative to the paper playbill one recieves prior to a show. 
 Curtain Call aims to both improve the theater experience for guests and reduce waste created by performance locations.
 
-## Shows 
-## Musical Numbers
-## Performer Bios 
+# Features v1.0
 
+- Curtain call will have a list of shows currently on tour across the USA
+- A user can click on a show and view Cast and Songs
--- scrape.py
+++ scrape.py
@@ -2,25 +2,42 @@ import requests
 from bs4 import BeautifulSoup
 import re
 from pprint import pprint
+import datetime
 
 def main():
     '''Description of the function'''
-    URL = "https://www.ibdb.com/Grosses/ViewProduction"
-    PARAMS = {
-    "IsNationalTour": True,
-    "IsBroadway": False,
-    "touringstartmonth": 10,
-    "touringstartday": 16,
-    "touringstartyear": 2021,
-    "touringendmonth": 10,
-    "touringendday": 17,
-    "touringendyear": 2021,
-    }
+#    now = datetime.datetime.now()
+#    last_week = now - datetime.timedelta(days=7)
+#    next_week = now + datetime.timedelta(days=7)
+#
+#    URL = "https://www.ibdb.com/Grosses/ViewProduction"
+#    PARAMS = {
+#    "IsNationalTour": True,
+#    "IsBroadway": False,
+#    "touringstartmonth": last_week.month,
+#    "touringstartday": last_week.day,
+#    "touringstartyear": last_week.year,
+#    "touringendmonth": next_week.month,
+#    "touringendday": next_week.day,
+#    "touringendyear": next_week.year,
+#    }
+#
+#    res = requests.post(URL, params=PARAMS)
+#    soup = BeautifulSoup(res.content, "html.parser")
+#    links = soup.find_all("a", href=re.compile("tour-production"))
+#
+#    BASE = "https://www.ibdb.com"
+#    show_links = [ BASE + link['href'] for link in links ]
+#    pprint(show_links)
 
-    res = requests.post(URL, params=PARAMS)
+    SHOW_URL = "https://www.ibdb.com/tour-production/come-from-away-518408#Songs"
+    res = requests.get(SHOW_URL)
     soup = BeautifulSoup(res.content, "html.parser")
-    links = soup.find_all("a", href=re.compile("tour-production"))
-    pprint(links)
+    song_id = soup.find("div", id=re.compile("Songs"))
+    songs = song_id.find_all("div", {"class": "col s6"})
+    title = [ song.text.strip() for song in songs ]
+    song_performers = tuple(zip(title[::2], title[1::2]))
+    pprint(song_performers)
 
 if __name__ == '__main__':
     main()
